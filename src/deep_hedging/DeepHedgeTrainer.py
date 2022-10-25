Fix the computational precision issues.

src/deep_hedging/DeepHedgeTrainer.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import Dict, Any, List, OrderedDict, Optional
 
 import torch
+from numpy.typing import NDArray
+from torch.optim.lr_scheduler import StepLR
 
 from src.deep_hedging.DeepHedge import DeepHedge
 from src.deep_hedging.objectives.HedgeObjective import HedgeObjective
@@ -19,8 +21,12 @@ class DeepHedgeTrainerConfig:
     def __post_init__(self):
         self.generation_adapters = self.generation_adapters if self.generation_adapters else AdapterList()
 
-    def reset_scheduler(self) -> None:
-        self.__post_init__()
+    def scheduler_from_optimizer(self, optimizer: torch.optim.Optimizer) -> StepLR:
+        return torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=self.scheduler_step_size,
+            gamma=self.scheduler_gamma,
+        )
 
 
 @dataclass
@@ -28,6 +34,10 @@ class HedgeMetrics(Metrics):
 
     profit_and_loss: torch.Tensor = None
     hedge_loss: torch.Tensor = None
+
+    @property
+    def loss(self) -> NDArray:
+        return self.hedge_loss.item()
 
     def create_print_dict(self) -> Dict[str, float]:
         return {'H-Loss': self.hedge_loss.item() if self.hedge_loss is not None else float('nan')}
@@ -51,12 +61,7 @@ class DeepHedgeTrainer(Trainer[HedgeMetrics]):
         self.dh = dh
         self.optimizer = torch.optim.Adam(self.dh.parameters())
         self.config = config
-
-        self.scheduler = torch.optim.lr_scheduler.StepLR(
-            self.optimizer,
-            step_size=self.config.scheduler_step_size,
-            gamma=self.config.scheduler_gamma,
-        )
+        self.scheduler = self.config.scheduler_from_optimizer(self.optimizer)
 
     def step(self, inputs: torch.Tensor) -> HedgeMetrics:
         m = HedgeMetrics()
@@ -69,7 +74,7 @@ class DeepHedgeTrainer(Trainer[HedgeMetrics]):
         self.dh.zero_grad()
         m.hedge_loss.backward()
         self.optimizer.step()
-        self.scheduler.step()
+        # self.scheduler.step()
 
         return m
 
@@ -79,8 +84,9 @@ class DeepHedgeTrainer(Trainer[HedgeMetrics]):
             batch_sizes: List[int],
             pbar_option: PbarOption = PbarOption.NO_BAR,
             pretrained: Optional[Any] = None,
+            loss_curve_address: Optional[Any] = None,
     ) -> OrderedDict[str, torch.Tensor]:
-        self.load_or_fit(f, batch_sizes, pbar_option, pretrained)
+        self.load_or_fit(f, batch_sizes, pbar_option, pretrained, loss_curve_address)
         return self.dh.state_dict()
 
     def load_module_from_state_dict(self, f: Any) -> None:

src/gan/RobustDhGan.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass, asdict, fields
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any, OrderedDict
 
+import numpy as np
 import torch
+from numpy.typing import NDArray
 
+from src.deep_hedging.DeepHedge import DeepHedge
 from src.deep_hedging.DeepHedgeTrainer import DeepHedgeTrainerConfig
+from src.generator.SdeGenerator import SdeGenerator
 from src.generator.SdeGeneratorTrainer import SdeGeneratorTrainerConfig
-from src.util.torch_util.TrainingUtil import Trainer, TrainerConfig, Metrics
+from src.util.torch_util.TrainingUtil import Trainer, TrainerConfig, Metrics, PbarOption
 
 
 @dataclass
@@ -16,7 +20,11 @@ class RobustDhGanMetrics(Metrics):
     generation_loss: torch.Tensor = None
 
     def create_print_dict(self) -> Dict[str, float]:
-        return {k: v.item() if v is not None else float('nan') for k, v in asdict(self).items()}
+        return {k: v.item() if v is not None else float('nan') for k, v in self.__dict__.items()}
+
+    @property
+    def loss(self) -> NDArray:
+        return np.array([v.item() if v is not None else float('nan') for v in self.__dict__.values()])
 
     @classmethod
     def summarize_metrics_list_to_metrics(cls, metric_list: List['RobustDhGanMetrics']) -> 'RobustDhGanMetrics':
@@ -31,13 +39,22 @@ class RobustDhGan(Trainer[RobustDhGanMetrics]):
 
     def __init__(
             self,
+            dh: DeepHedge,
             hedge_config: DeepHedgeTrainerConfig,
+            gen: SdeGenerator,
             gen_config: SdeGeneratorTrainerConfig,
             trainer_config: TrainerConfig,
     ):
         super().__init__(trainer_config)
-        self.hedge_config = hedge_config
+        self.dh = dh
+        self.dh_optimizer = torch.optim.Adam(self.dh.parameters())
+        self.dh_config = hedge_config
+        self.dh_scheduler = self.dh_config.scheduler_from_optimizer(self.dh_optimizer)
+
+        self.gen = gen
+        self.gen_optimizer = torch.optim.Adam(self.gen.parameters())
         self.gen_config = gen_config
+        self.gen_scheduler = self.gen_config.scheduler_from_optimizer(self.gen_optimizer)
 
         self._train_hedge = True
         self._train_generator = True
@@ -54,18 +71,18 @@ class RobustDhGan(Trainer[RobustDhGanMetrics]):
             return hedge_loss
 
     def update_deep_hedge(self, hedge_loss: torch.Tensor) -> None:
-        self.hedge_config.deep_hedge.zero_grad()
+        self.dh.zero_grad()
         hedge_loss.backward()
-        self.hedge_config.optimizer.step()
-        self.hedge_config.scheduler.step()
+        self.dh_optimizer.step()
+        # self.dh_scheduler.step()
 
     def compute_hedge_loss(self, noise: torch.Tensor) -> torch.Tensor:
-        generated = self.gen_config.generator(noise)
+        generated = self.gen(noise)
         return self.compute_hedge_loss_from_generated(generated)
 
     def compute_hedge_loss_from_generated(self, generated: torch.Tensor) -> torch.Tensor:
-        profit_and_loss = self.hedge_config.deep_hedge(self.hedge_config.generation_adapters(generated))
-        return self.hedge_config.hedge_objective(profit_and_loss)
+        profit_and_loss = self.dh(self.dh_config.generation_adapters(generated))
+        return self.dh_config.hedge_objective(profit_and_loss)
 
     def generator_step(self, noise) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor]]:
         if self._train_generator:
@@ -76,16 +93,31 @@ class RobustDhGan(Trainer[RobustDhGanMetrics]):
         return None, None
 
     def update_generator(self, generation_loss: torch.Tensor) -> None:
-        self.gen_config.generator.zero_grad()
+        self.gen.zero_grad()
         generation_loss.backward()
-        self.gen_config.optimizer.step()
-        self.gen_config.scheduler.step()
+        self.gen_optimizer.step()
+        # self.gen_scheduler.step()
 
     def compute_generation_loss(self, noise: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        generated = self.gen_config.generator(noise)
+        generated = self.gen(noise)
         updated_hedge_loss = self.compute_hedge_loss_from_generated(generated) if self._train_hedge else 0.0
         penalty = self.gen_config.penalizer(self.gen_config.penalization_adapters(generated))
         return (penalty - updated_hedge_loss), penalty
+
+    def load_module_from_state_dict(self, f: Any) -> None:
+        self.load_state_dict(torch.load(f))
+
+    def get_weights_from_load_and_fitting_procedure(
+            self,
+            f: Any,
+            batch_sizes: List[int],
+            pbar_option: PbarOption = PbarOption.NO_BAR,
+            pretrained: Optional[Any] = None,
+            loss_curve_address: Optional[Any] = None,
+            parameter_tracking_address: Optional[Any] = None,
+    ) -> OrderedDict[str, torch.Tensor]:
+        self.load_or_fit(f, batch_sizes, pbar_option, pretrained, loss_curve_address, parameter_tracking_address)
+        return self.state_dict()
 
     def deactivate_generation_training(self) -> None:
         self._train_generator = False

src/generator/HestonCoefficient.py
@@ -23,9 +23,33 @@ class HestonDriftCoefficient(Coefficient[HestonCoefficientConfig]):
     def __init__(self, config: HestonCoefficientConfig):
         super().__init__(config)
 
-        self.drift = nn.Parameter(torch.tensor(self.config.initializer.drift, dtype=torch.float32))
-        self.reversion_speed = nn.Parameter(torch.tensor(self.config.initializer.reversion_speed, dtype=torch.float32))
-        self.reversion_level = nn.Parameter(torch.tensor(self.config.initializer.reversion_level, dtype=torch.float32))
+        self._drift = nn.Parameter(torch.tensor(
+            self.config.initializer.normalize_drift(self.config.initializer.drift),
+            dtype=torch.float32,
+        ))
+        self._reversion_speed = nn.Parameter(torch.tensor(self.config.initializer.reversion_speed, dtype=torch.float32))
+        self._reversion_speed = nn.Parameter(torch.tensor(
+            self.config.initializer.normalize_reversion_speed(self.config.initializer.reversion_speed),
+            dtype=torch.float32,
+        ))
+        self._reversion_level = nn.Parameter(torch.tensor(
+            self.config.initializer.normalize_reversion_level(self.config.initializer.reversion_level),
+            dtype=torch.float32,
+        ))
+
+    @property
+    def drift(self) -> torch.Tensor:
+        return self.config.initializer.denormalize_drift(self._drift)
+
+    @property
+    def reversion_speed(self) -> torch.Tensor:
+        return self.config.initializer.denormalize_reversion_speed(self._reversion_speed)
+
+    @property
+    def reversion_level(self) -> torch.Tensor:
+        return self.config.initializer.denormalize_reversion_level(
+            torch.clamp(self._reversion_level, torch.tensor([0]))
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.eliminate_time_if_time_invariant(x)
@@ -44,12 +68,15 @@ class HestonDiffusionCoefficient(Coefficient[HestonCoefficientConfig]):
     def __init__(self, config: HestonCoefficientConfig):
         super().__init__(config)
 
-        self._vol_of_vol = nn.Parameter(torch.tensor(self.config.initializer.vol_of_vol, dtype=torch.float32))
+        self._vol_of_vol = nn.Parameter(torch.tensor(
+            self.config.initializer.normalize_vol_of_vol(self.config.initializer.vol_of_vol),
+            dtype=torch.float32,
+        ))
         self._correlation = nn.Parameter(torch.tensor(self.config.initializer.correlation, dtype=torch.float32))
 
     @property
     def vol_of_vol(self):
-        return torch.clamp(self._vol_of_vol, torch.tensor([0]))
+        return self.config.initializer.denormalize_vol_of_vol(torch.clamp(self._vol_of_vol, torch.tensor([0])))
 
     @property
     def correlation(self):

src/generator/SdeGeneratorTrainer.py
@@ -1,22 +1,92 @@
 from dataclasses import dataclass
+from typing import List, Dict, Any, Optional, OrderedDict
 
 import torch
+from numpy.typing import NDArray
+from torch.optim.lr_scheduler import StepLR
 
 from src.generator.SdeGenerator import SdeGenerator
 from src.penalty.Metric import Metric
 from src.util.torch_util.AdapterUtil import AdapterList
+from src.util.torch_util.TrainingUtil import Trainer, Metrics, _Metrics, TrainerConfig, PbarOption
 
 
 @dataclass
 class SdeGeneratorTrainerConfig:
-    generator: SdeGenerator
     penalizer: Metric
-    optimizer: torch.optim.Optimizer
     penalization_adapters: AdapterList = None
+    scheduler_step_size: int = 100
+    scheduler_gamma: float = 0.9
 
     def __post_init__(self):
         self.penalization_adapters = self.penalization_adapters if self.penalization_adapters else AdapterList()
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=100, gamma=0.9)
 
-    def reset_scheduler(self) -> None:
-        self.__post_init__()
+    def scheduler_from_optimizer(self, optimizer: torch.optim.Optimizer) -> StepLR:
+        return torch.optim.lr_scheduler.StepLR(
+            optimizer,
+            step_size=self.scheduler_step_size,
+            gamma=self.scheduler_gamma,
+        )
+
+
+@dataclass
+class SdeGeneratorMetrics(Metrics):
+
+    generation_loss: torch.Tensor = None
+
+    def create_print_dict(self) -> Dict[str, float]:
+        return {'G-Loss': self.generation_loss.item() if self.generation_loss is not None else float('nan')}
+
+    @property
+    def loss(self) -> NDArray:
+        return self.generation_loss.item()
+
+    @classmethod
+    def summarize_metrics_list_to_metrics(cls, metrics_list: List['SdeGeneratorMetrics']) -> 'SdeGeneratorMetrics':
+        return cls(
+            generation_loss = None if metrics_list[0].generation_loss is None else torch.mean(
+                torch.tensor([metr.generation_loss for metr in metrics_list])
+            ),
+        )
+
+
+class SdeGeneratorTrainer(Trainer[SdeGeneratorMetrics]):
+
+    def __init__(
+            self,
+            gen: SdeGenerator,
+            gen_config: SdeGeneratorTrainerConfig,
+            trainer_config: TrainerConfig,
+    ):
+        super().__init__(trainer_config)
+
+        self.gen = gen
+        self.gen_optimizer = torch.optim.Adam(self.gen.parameters())
+        self.gen_config = gen_config
+        self.gen_scheduler = self.gen_config.scheduler_from_optimizer(self.gen_optimizer)
+
+    def step(self, noise: torch.Tensor) -> _Metrics:
+        generated = self.gen(noise)
+        generation_loss = self.gen_config.penalizer(self.gen_config.penalization_adapters(generated))
+
+        self.gen.zero_grad()
+        generation_loss.backward()
+        self.gen_optimizer.step()
+        # self.gen_scheduler.step()
+
+        return SdeGeneratorMetrics(generation_loss)
+
+    def get_weights_from_load_and_fitting_procedure(
+            self,
+            f: Any,
+            batch_sizes: List[int],
+            pbar_option: PbarOption = PbarOption.NO_BAR,
+            pretrained: Optional[Any] = None,
+            loss_curve_address: Optional[Any] = None,
+            parameter_tracking_address: Optional[Any] = None,
+    ) -> OrderedDict[str, torch.Tensor]:
+        self.load_or_fit(f, batch_sizes, pbar_option, pretrained, loss_curve_address, parameter_tracking_address)
+        return self.gen.state_dict()
+
+    def load_module_from_state_dict(self, f: Any) -> None:
+        self.gen.load_state_dict(torch.load(f))

src/penalty/Augmentations.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 
+import numpy as np
 import torch
 
 from src.config import DEVICE
@@ -93,4 +94,4 @@ class LeadLagWithTime(BaseAugmentation):
 
 
 def get_time_steps(batch_size: int, td: TimeDiscretization) -> torch.Tensor:
-    return torch.as_tensor(td.times, dtype=torch.float32, device=DEVICE)[None, :, None].repeat(batch_size, 1, 1)
+    return torch.from_numpy(np.copy(td.times))[None, :, None].repeat(batch_size, 1, 1)

src/util/processes/HestonGenerator.py
@@ -15,6 +15,30 @@ class HestonParameterSet:
     vol_of_vol: float
     correlation: float
 
+    def normalize_drift(self, drift):
+        return drift / self.drift if self.drift != 0 else self.drift
+
+    def denormalize_drift(self, normalized_drift):
+        return normalized_drift * self.drift if self.drift != 0 else self.drift
+
+    def normalize_reversion_speed(self, reversion_speed):
+        return reversion_speed / self.reversion_speed
+
+    def denormalize_reversion_speed(self, normalized_reversion_speed):
+        return normalized_reversion_speed * self.reversion_speed
+
+    def normalize_reversion_level(self, reversion_level):
+        return reversion_level / self.reversion_level
+
+    def denormalize_reversion_level(self, normalized_reversion_level):
+        return normalized_reversion_level * self.reversion_level
+
+    def normalize_vol_of_vol(self, vol_of_vol):
+        return vol_of_vol / self.vol_of_vol
+
+    def denormalize_vol_of_vol(self, normalized_vol_of_vol):
+        return normalized_vol_of_vol * self.vol_of_vol
+
 
 class HestonGenerator(EulerMaruyamaGenerator):
 
@@ -54,4 +78,4 @@ class HestonGenerator(EulerMaruyamaGenerator):
 
     def get_correction_term(self, ttm: NDArray, volatility_process: NDArray) -> NDArray:
         return (volatility_process - self.pars.reversion_level) / self.pars.reversion_speed \
-            * (1 - np.exp(- self.pars.reversion_speed * ttm)) + self.pars.reversion_level * ttm
+               * (1 - np.exp(- self.pars.reversion_speed * ttm)) + self.pars.reversion_level * ttm

src/util/torch_util/TrainingUtil.py
@@ -2,6 +2,9 @@ from abc import abstractmethod, ABCMeta
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Callable, Optional, Dict, TypeVar, Generic, get_args, Any
+
+import numpy as np
+from numpy.typing import NDArray
 from tqdm.auto import tqdm
 
 import torch
@@ -22,6 +25,11 @@ class Metrics(metaclass=ABCMeta):
 
     @abstractmethod
     def create_print_dict(self) -> Dict[str, float]:
+        pass
+
+    @property
+    @abstractmethod
+    def loss(self) -> NDArray:
         pass
 
     @classmethod
@@ -80,11 +88,13 @@ class Trainer(torch.nn.Module, Generic[_Metrics], metaclass=ABCMeta):
             batch_sizes: List[int],
             pbar_option: PbarOption = PbarOption.VANISHING_BATCH_BAR,
             pretrained: Optional[Any] = None,
+            loss_curve_address: Optional[Any] = None,
+            parameter_tracking_address: Optional[Any] = None,
     ) -> None:
         try:
             self.load_module_from_state_dict(f)
         except FileNotFoundError:
-            self.fit(batch_sizes, pbar_option, pretrained)
+            self.fit(batch_sizes, pbar_option, pretrained, loss_curve_address, parameter_tracking_address)
 
     @abstractmethod
     def load_module_from_state_dict(self, f: Any) -> None:
@@ -95,6 +105,8 @@ class Trainer(torch.nn.Module, Generic[_Metrics], metaclass=ABCMeta):
             batch_sizes: List[int],
             pbar_option: PbarOption = PbarOption.VANISHING_BATCH_BAR,
             pretrained: Optional[Any] = None,
+            loss_curve_address: Optional[Any] = None,
+            parameter_tracking_address: Optional[Any] = None,
     ) -> None:
         if pretrained:
             self.load_module_from_state_dict(pretrained)
@@ -105,11 +117,21 @@ class Trainer(torch.nn.Module, Generic[_Metrics], metaclass=ABCMeta):
         ) if pbar_option.has_epoch_bar else enumerate(batch_sizes)
 
         inputs = None
+        losses = []
+        parameters_for_epoch = []
         for epoch, batch_size in bs_pbar:
             inputs = self.generate_inputs(inputs, epoch)
             metrics = self.fit_on_batches(inputs, batch_size, pbar_option)
             bs_pbar.set_postfix(metrics.create_print_dict()) if pbar_option.has_epoch_bar else None
+            losses.append(metrics.loss)
+            parameters_for_epoch.append({k: v.clone().detach().numpy() for k, v in self.named_parameters()})
             self.counter += 1
+
+        if loss_curve_address:
+            np.save(loss_curve_address, np.stack(losses, axis=0))
+
+        if parameter_tracking_address:
+            np.save(parameter_tracking_address, parameters_for_epoch)
 
     def fit_on_batches(self, inputs: torch.Tensor, batch_size: int, pbar_option: PbarOption) -> Metrics:
         batches = self.batch_inputs(inputs, batch_size)