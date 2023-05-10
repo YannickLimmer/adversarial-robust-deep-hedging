e01e5444fd47a4c2ff2a37ca7eac69a9c2d5a8a7
yannicklimmer <yannick.limmer@icloud,com>
2023-05-10 13:20:29 +0100

    Add penalty of the Hermann-MuhleKarbe-Seifried paper.

--- src/penalty/CompareVolatility.py
+++ src/penalty/CompareVolatility.py
@@ -1,12 +1,16 @@
 import functools
 from dataclasses import dataclass
+from typing import Callable
 
 import numpy as np
 import torch
 
 from src.config import DEVICE
+from src.deep_hedging.DeepHedge import DeepHedge
+from src.deep_hedging.objectives.HedgeObjective import HedgeObjective
 from src.penalty.Metric import Metric, MetricConfig
 from src.util.TimeUtil import TimeDiscretization
+from src.util.torch_util.AdapterUtil import ConvertToIncrements
 
 
 @dataclass
@@ -33,3 +37,36 @@ class CompareVolatility(Metric[VolatilityComparisonConfig]):
     def to_volatility(self, arr: torch.Tensor) -> torch.Tensor:
         log_incr = torch.diff(torch.log(arr), 1, 1)
         return torch.std((log_incr - torch.mean(log_incr, dim=0)) / self.config.time_root)
+
+
+@dataclass
+class DynamicVolatilityComparisonConfig(VolatilityComparisonConfig):
+    hedge_objective: HedgeObjective
+
+
+class CompareVolatilityDynamically(Metric[DynamicVolatilityComparisonConfig]):
+
+    def __init__(
+            self,
+            original: torch.Tensor,
+            dh: DeepHedge,
+            metric_config: DynamicVolatilityComparisonConfig,
+            transform: Callable[[torch.Tensor], torch.Tensor] = None,
+    ):
+        super().__init__(original, metric_config, transform=transform)
+        self.dh = dh
+        self.converter = ConvertToIncrements()
+
+    def forward(self, generated: torch.Tensor) -> torch.Tensor:
+        realized_vol = self.to_volatility(generated)
+        hedge_objective = self.config.hedge_objective(self.dh(self.converter(generated)))
+        return self.transform(hedge_objective * (self.original_volatility - realized_vol) ** 2)
+
+    def to_volatility(self, arr: torch.Tensor) -> torch.Tensor:
+        log_incr = torch.diff(torch.log(arr), 1, 1)
+        return torch.std((log_incr - torch.mean(log_incr, dim=0)) / self.config.time_root)
+
+    @property
+    @functools.lru_cache()
+    def original_volatility(self) -> torch.Tensor:
+        return self.to_volatility(self.original)
--- src/penalty/CompareVolatilityTest.py
+++ src/penalty/CompareVolatilityTest.py
@@ -1,29 +1,59 @@
 import numpy as np
+import torch
 
+from src.config import DEVICE
+from src.deep_hedging.AbstractDeepHedge import DeepHedgeConfig
+from src.deep_hedging.DeepHedge import DeepHedge
+from src.deep_hedging.StrategyNet import StrategyNetConfig
+from src.deep_hedging.objectives.HedgeObjective import Entropy, StableEntropy, MeanVariance
 from src.derivative.EuropeanCallOption import EuropeanCallOption
-from src.penalty.CompareVolatility import CompareVolatility, VolatilityComparisonConfig
+from src.penalty.CompareVolatility import CompareVolatility, VolatilityComparisonConfig, CompareVolatilityDynamically, \
+    DynamicVolatilityComparisonConfig
 from src.util.TimeUtil import UniformTimeDiscretization
 from src.util.processes.BlackScholesGenerator import BlackScholesGenerator, BlackScholesParameterSet
+from src.util.torch_util.AdapterUtil import ConvertToIncrements
+
+trading_freq: int = 5
+td = UniformTimeDiscretization(trading_freq * 1. / 255., 90 // trading_freq)
+bs_parameters = BlackScholesParameterSet(
+    drift=0.0,
+    sigma=0.2,
+)
+bs_generator = BlackScholesGenerator(
+    drift=np.array([bs_parameters.drift]), sigma=np.array([bs_parameters.sigma]),
+).provide_generator(
+    initial_value=np.array([1.0]),
+    times=td.times,
+    random_number_generator=np.random.default_rng(404)
+)
+bs_process = bs_generator(100000)
 
 
 def test_bs_process_vola():
-
-    trading_freq: int = 5
-    td = UniformTimeDiscretization(trading_freq * 1. / 255., 90 // trading_freq)
-    bs_parameters = BlackScholesParameterSet(
-        drift=0.0,
-        sigma=0.2,
-    )
-    bs_generator = BlackScholesGenerator(
-        drift=np.array([bs_parameters.drift]), sigma=np.array([bs_parameters.sigma]),
-    ).provide_generator(
-        initial_value=np.array([1.0]),
-        times=td.times,
-        random_number_generator=np.random.default_rng(404)
-    )
-    bs_process = bs_generator(100000)
 
     config = VolatilityComparisonConfig(td)
     p = CompareVolatility(bs_process, config)
 
     p(bs_process)
+
+
+def test_dynamic_comparison():
+
+    derivative = EuropeanCallOption(strike=1.0, time_discretization=td, price=0.0)
+    dh = DeepHedge(
+        DeepHedgeConfig(
+            derivative=derivative,
+            initial_information_value=torch.tensor([1.0], dtype=torch.float32, device=DEVICE),
+            strategy_config=StrategyNetConfig(
+                dim_of_information_process=1,
+                dim_of_tradable_asset=1,
+                number_of_layers=2,
+                nodes_in_intermediate_layers=128,
+            ),
+        )
+    )
+
+    config = DynamicVolatilityComparisonConfig(td, hedge_objective=MeanVariance(130))
+    p = CompareVolatilityDynamically(bs_process, dh, config)
+    assert p(bs_process).item() == 0
+    assert p(bs_generator(100)).item() > 0
