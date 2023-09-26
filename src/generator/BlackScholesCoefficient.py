5555e1fc6e706d3369201fd7b6868ebd8f908248
yannicklimmer <yannick.limmer@icloud.com>
2023-09-26 11:55:55 +0100

    Multi-dimensional BlackScholes generator.

--- src/generator/BlackScholesCoefficient.py
+++ src/generator/BlackScholesCoefficient.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 import torch
+from arch.typing import NDArray
 from torch import nn
 
 from src.config import DEVICE
@@ -50,3 +51,49 @@ class BlackScholesDiffusionCoefficient(Coefficient[BlackScholesCoefficientConfig
         x = self.eliminate_time_if_time_invariant(x)
         return (x * self.sigma)[:, :, None]
 
+
+@dataclass
+class MdBlackScholesCoefficientConfig(CoefficientConfig):
+    drift_vector: NDArray
+    cov_matrix: NDArray
+    initial_asset_price: NDArray
+
+    time_invariant: bool = field(default=True, init=False)
+
+
+class MdBlackScholesDriftCoefficient(Coefficient[MdBlackScholesCoefficientConfig]):
+
+    def __init__(self, config: MdBlackScholesCoefficientConfig):
+        super().__init__(config)
+        self.drift = nn.Parameter(torch.Tensor(
+            self.config.drift_vector,
+            dtype=torch.float32,
+            device=DEVICE,
+        ))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.eliminate_time_if_time_invariant(x)
+        return x * self.drift
+
+    def get_initial_asset_price(self) -> torch.Tensor:
+        return nn.Parameter(torch.Tensor(
+            self.config.initial_asset_price,
+            dtype=torch.float32,
+            device=DEVICE,
+        ))
+
+
+class MdBlackScholesDiffusionCoefficient(Coefficient[MdBlackScholesCoefficientConfig]):
+
+    def __init__(self, config: BlackScholesCoefficientConfig):
+        super().__init__(config)
+        self.cov_matrix  = nn.Parameter(
+            torch.Tensor(
+                self.config.cov_matrix,
+                dtype=torch.float32,
+                device=DEVICE,
+        ))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.eliminate_time_if_time_invariant(x)
+        return x * self.sigma
